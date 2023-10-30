8366d9187685f6d5f08b41104ba16d50ad144a9f
ZhiLiu724 <zl724@cornell.edu>
2023-10-30 14:40:40 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import os
 
-if ___name___ == "__main__":
-    print('-------Getting team names-------')
-    folders = [f for f in os.listdir('../agents') if not os.path.isfile(os.path.join('../agents', f))]
-    folders = [f for f in folders if '_' not in f]
-    assert len(folders) == 1, "There should be only one folder in the agents directory, or that your team name contains an underscore."
 
-    print("Hello World!")
+print('-------Getting team names-------')
+folders = [f for f in os.listdir('../agents') if not os.path.isfile(os.path.join('../agents', f))]
+folders = [f for f in folders if '_' not in f]
+assert len(folders) == 1, "There should be only one folder in the agents directory, or that your team name contains an underscore."
+team_name = folders[0]
+print('Team name: {}, running test script'.format(team_name))
+
+print("Hello World!")
