298773dadb2c4bee1ff4ab1cce145d9c6afd4052
ZhiLiu724 <zl724@cornell.edu>
2023-10-30 14:39:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello tester')
+import os
+
+if ___name___ == "__main__":
+    print('-------Getting team names-------')
+    folders = [f for f in os.listdir('../agents') if not os.path.isfile(os.path.join('../agents', f))]
+    folders = [f for f in folders if '_' not in f]
+    assert len(folders) == 1, "There should be only one folder in the agents directory, or that your team name contains an underscore."
+
+    print("Hello World!")
