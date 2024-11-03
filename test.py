561b911fc9463b3447ba52176ff53a2040312419
Zhi Liu <zl724@cornell.edu>
2024-11-03 15:07:46 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import make_env
+import make_env_2024 as make_env
 
 print('-------Getting team names-------')
 current_script_path = os.path.abspath(__file__)
